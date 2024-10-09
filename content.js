0c3f26a9a31e296a0a64a5e17b74375311c356ad
llasNicho <nichollas.schaurich@aluno.educa.go.gov.br>
2024-10-09 08:48:17 -0300

    add: content.js code

--- content.js
+++ content.js
@@ -1,7 +1,31 @@
-// content.js
-// Author:
-// Author URI: https://
-// Author Github URI: https://www.github.com/
-// Project Repository URI: https://github.com/
-// Description: Handles all the webpage level activities (e.g. manipulating page data, etc.)
-// License: MIT
+const sleepNow = (delay) => new Promise((resolve) => setTimeout(resolve,delay)); // sleep function
+
+do {
+    await sleepNow(100)
+
+    if (!document.URL.search("https://sergoias.portal.sagreseduca.com.br/challenges/")) {break}
+    if (document == null || document == undefined) {break}
+
+    console.log("site do sergoias")
+
+    var wrongAlternatives = document.getElementsByClassName("h5p-iframe")[0]
+        
+    if (wrongAlternatives != null && wrongAlternatives != undefined) {
+        wrongAlternatives = wrongAlternatives.contentWindow.document.firstElementChild.getElementsByTagName("body")[0]
+       
+        if (wrongAlternatives != null && wrongAlternatives != undefined) {
+            wrongAlternatives = wrongAlternatives.getElementsByClassName("h5p-sc-alternatives")[0]
+            
+            if (wrongAlternatives != null && wrongAlternatives != undefined) {
+                wrongAlternatives = wrongAlternatives.getElementsByClassName("h5p-sc-is-wrong")
+
+                if (wrongAlternatives.length > 0 && wrongAlternatives != undefined && wrongAlternatives != null) {
+                    for (const answer of wrongAlternatives) {
+                        answer.outerHTML = "";
+                        answer.innerHTML = "";
+                    }
+                }
+            } 
+        }
+    }
+} while (true)
